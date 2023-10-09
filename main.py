6f886d1a4ce1b4228dd1977a11535d786160d595
Jip <jipvanbuitenen@gmail.com>
2023-10-09 14:17:12 +0200

    added api key and converted to datetime

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ import streamlit as st
 # load data
 laadpaaldata = pd.read_csv('laadpaaldata.csv')
 voertuigen = pd.read_csv('elektrischeVoertuigen.csv')
-# TODO change request (maxresults, key)
-res = requests.get("https://api.openchargemap.io/v3/poi/?output=json&countrycode=NL&maxresults=100&compact=true&verbose=false&key=93b912b5-9d70-4b1f-960b-fb80a4c9c017")
+key = "b2451f45-7af1-4a46-a430-8496d6583401"
+res = requests.get(f"https://api.openchargemap.io/v3/poi/?output=json&countrycode=NL&maxresults=10000&compact=true&verbose=false&key={key}")
 data = res.json()
 openchargemap = pd.json_normalize(data)
 df4 = pd.json_normalize(openchargemap.Connections)
 df5 = pd.json_normalize(df4[0])
 openchargemap = pd.concat([openchargemap, df5], axis=1)
 openchargemap.drop(columns=['Connections'], inplace=True)
-
 # TODO clean data
-
+laadpaaldata['Started'] = pd.to_datetime(laadpaaldata['Started'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+laadpaaldata['Ended'] = pd.to_datetime(laadpaaldata['Ended'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+laadpaaldata.dropna(subset=['Started'], inplace=True)
+laadpaaldata.dropna(subset=['Ended'], inplace=True)
 # TODO drop unnecessary columns
 
 # TODO merge data
