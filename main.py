54f411c3c5f248f238138c660649ceda073873b0
Valentijn Kilian <valentijnkilian@gmail.com>
2023-10-09 22:54:52 +0200

    Feature Engineering & improvements of visual metrics

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ st.set_page_config(
 
 # load data
 laadpaaldata = pd.read_csv('laadpaaldata.csv')
-voertuigen = pd.read_csv('elektrischeVoertuigen.csv')
+# voertuigen = pd.read_csv('elektrischeVoertuigen.csv')
 key = "b2451f45-7af1-4a46-a430-8496d6583401"
 res = requests.get(
     f"https://api.openchargemap.io/v3/poi/?output=json&countrycode=NL&maxresults=10000&compact=true&verbose=false&key={key}")
@@ -29,31 +29,54 @@ laadpaaldata['Ended'] = pd.to_datetime(laadpaaldata['Ended'], format='%Y-%m-%d %
 laadpaaldata.dropna(subset=['Started'], inplace=True)
 laadpaaldata.dropna(subset=['Ended'], inplace=True)
 
+# Drop rows with negative charging times
+rows_to_drop = laadpaaldata[((laadpaaldata['ChargeTime']) < 0)].index
+laadpaaldata.drop(index=rows_to_drop, inplace=True)
+
+# Convert power in Watt to kiloWatt
+laadpaaldata['TotalEnergy'] = round((laadpaaldata['TotalEnergy'] / 1000), 2)
+laadpaaldata['MaxPower'] = round(laadpaaldata['MaxPower'] / 1000, 2)
+# Efficiency calculation
+laadpaaldata['Efficiency'] = (round((laadpaaldata['ChargeTime'] / laadpaaldata['ConnectedTime']) * 100, 2))
+
+print(laadpaaldata.agg({'Efficiency': ['max', 'min', 'mean']}))
+
+
 # TODO drop unnecessary columns
 
 # TODO merge data
 
 # Streamlit dashboard code
 st.title('Elektrische mobiliteit & laadpalen')
-print(laadpaaldata.mean())
+
 # Container 1: Metrics
-metric1, metric2, metric3 = st.columns(3)
-charge_time = laadpaaldata['ChargeTime'].mean()
-connected_time = laadpaaldata['ConnectedTime'].mean()
+metric1, metric2, metric3, metric4 = st.columns(4)
+# Get average charging time
+charge_time = (laadpaaldata['ChargeTime'].mean()) * 60
+connected_time = laadpaaldata['ConnectedTime'].mean() * 60
+# Convert to hours and minutes
+hours_charge, minutes_charge = divmod(charge_time, 60)
+hours_conn, minutes_conn = divmod(connected_time, 60)
+
+
 consumption = laadpaaldata['MaxPower'].mean()
+avg_eff = laadpaaldata['Efficiency'].mean()
 metric1.metric(
-    label='Avg. charge time',
-    value=f" {round(charge_time, 2)} uur"
+    label='Gem. Laadtijd',
+    value=f" {int(hours_charge)} uur {int(minutes_charge)} min"
 )
 metric2.metric(
-    label='Avg. connected Time',
-    value=f" {round(connected_time, 2)} uur"
+    label='Gem. Tijd aan de laadpaal',
+    value=f" {int(hours_conn)} uur {int(minutes_conn)} min"
 )
 metric3.metric(
-    label='Avg. Consumption (kWh)',
+    label='Gem. Efficientie',
+    value=f"{round(avg_eff, 2)} %"
+)
+metric4.metric(
+    label='Gem. Verbruik (kWh)',
     value=f" {round(consumption, 2)} kWh"
 )
-
 # Container 2: Charts
 fig_col1, fig_col2 = st.columns(2)
 df_grouped_day = laadpaaldata.groupby(pd.Grouper(key='Started', freq='D')).sum()
@@ -64,4 +87,4 @@ with fig_col2:
     st.markdown('### Second Chart')
     st.bar_chart(df_grouped_day, y='MaxPower')
 
-st.scatter_chart(df_grouped_day)
+st.bar_chart(laadpaaldata, x='TotalEnergy', y='Efficiency')
