827ab2d153b700db861a7219b532f343ea58c92e
Valentijn Kilian <valentijnkilian@gmail.com>
2023-10-09 16:39:56 +0200

    Added: page (sidebar) layout

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import numpy as np
 import plotly.express as px
 import matplotlib.pyplot as plt
 import streamlit as st
+from st_pages import Page, Section, show_pages, add_page_title
+
+add_page_title()
+show_pages(
+    [
+        Page('main.py', 'Home'),
+        Section('Data Reports'),
+        Page('pages/laadpaaldata_report.py', 'Laadpaal data report'),
+        Page('pages/elektrischevoertuigendata_report.py', 'Elektrische Voertuigen data report')
+    ]
+)
+
 
 # load data
 laadpaaldata = pd.read_csv('laadpaaldata.csv')
@@ -21,9 +33,7 @@ laadpaaldata['Started'] = pd.to_datetime(laadpaaldata['Started'], format='%Y-%m-
 laadpaaldata['Ended'] = pd.to_datetime(laadpaaldata['Ended'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
 laadpaaldata.dropna(subset=['Started'], inplace=True)
 laadpaaldata.dropna(subset=['Ended'], inplace=True)
+
 # TODO drop unnecessary columns
 
 # TODO merge data
-
-# Streamlit code
-
